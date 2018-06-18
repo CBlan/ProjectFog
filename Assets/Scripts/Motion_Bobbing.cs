5e5b01d17c71a47e42cd0a98989fd6227873267c
CBlan <cblan002@gmail.com>
2018-06-18 14:42:35 +1200

    dash done

---
+++ Assets/Scripts/Motion_Bobbing.cs
@@ -0,0 +1,39 @@
+﻿// bob an object up and down smoothly
+
+using UnityEngine;
+using System.Collections;
+
+public class Motion_Bobbing : MonoBehaviour {
+
+    private float waveAngle = 0.0f;
+    private float initialY;
+
+    public float amplitude = 0.25f;
+    public float frequency = 0.4f;
+
+    void Start()
+    {
+        //initialY = transform.position.y;
+    }
+
+    private void OnEnable()
+    {
+        initialY = transform.localPosition.y;
+    }
+
+    void Update ()
+    {
+        idleBob();
+	}
+
+    void idleBob()
+    {
+        waveAngle += Time.deltaTime * frequency;
+
+        float x = transform.localPosition.x;
+        float y = initialY + amplitude * Mathf.Cos(waveAngle + frequency);
+        float z = transform.localPosition.z;
+
+        transform.localPosition = new Vector3(x, y, z);
+    }
+}
---
+++ Assets/Scripts/Motion_Shake.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion_Shake : MonoBehaviour {
+
+    public bool shakeEnabled = true;
+    public float shake = 0f;
+    public float shakeAmount = 0.5f;
+    float decreaseFactor = 1.0f;
+    private Vector3 originalPos;
+    private bool isShaking;
+ 
+    void Update()
+    {
+        if(enabled)
+        {
+
+            if (shake > 0)
+            {
+                transform.position = originalPos + Random.insideUnitSphere * shakeAmount;
+                shake -= Time.deltaTime * decreaseFactor;
+            }
+            else if (shake < 0)
+            {
+                shake = 0f;
+                transform.position = originalPos;
+                isShaking = false;
+            }
+        }
+    }
+
+    public void Shake(float magnitude, float time)
+    {
+        if (!isShaking)
+        {
+            isShaking = true;
+            shakeAmount = magnitude;
+            shake = time;
+            originalPos = transform.position;
+        }
+
+    }
+}
