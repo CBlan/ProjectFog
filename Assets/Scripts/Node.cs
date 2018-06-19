8ed0dba96680513eca1a071196d83c02b4945a7d
cblan <cblan002@gmail.com>
2018-06-19 15:42:55 +1200

    Pathfinding Started. up to get node distance

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -6,10 +6,27 @@ public class Node {
 
     public bool walkable;
     public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
+    public int gridZ;
 
-    public Node(bool _walkable, Vector3 _worldPos)
+    public int gCost;
+    public int hCost;
+
+    public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY, int _gridZ)
     {
         walkable = _walkable;
         worldPosition = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
+        gridZ = _gridZ;
+    }
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
     }
 }
--- Assets/Scripts/NodeGrid.cs
+++ Assets/Scripts/NodeGrid.cs
@@ -15,8 +15,11 @@ public class NodeGrid : MonoBehaviour {
     float nodeDiameter;
     int gridSizeX, gridSizeY, gridSizeZ;
 
+    public static NodeGrid nodeGrid;
+
     private void Start()
     {
+        nodeGrid = this;
         nodeDiameter = nodeRadius * 2;
         gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
@@ -39,10 +42,40 @@ public class NodeGrid : MonoBehaviour {
                 {
                     Vector3 worldPoint = worldBottomBottomLeft + (Vector3.right * (x * nodeDiameter + nodeRadius)) + (Vector3.up * (y * nodeDiameter + nodeRadius)) + (Vector3.forward * (z * nodeDiameter + nodeRadius));
                     bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-                    grid[x, y, z] = new Node(walkable, worldPoint);
+                    grid[x, y, z] = new Node(walkable, worldPoint, x, y, z);
                 }
             }
         }
+    }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                for (int z = -1; z <= 1; z++)
+                {
+                    if (x == 0 && y == 0 && z == 0)
+                    {
+                        continue;
+                    }
+
+                    int checkX = node.gridX + x;
+                    int checkY = node.gridY + y;
+                    int checkZ = node.gridZ + z;
+
+                    if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY && checkZ >= 0 && checkZ < gridSizeZ)
+                    {
+                        neighbours.Add(grid[checkX, checkY, checkZ]);
+                    }
+                }
+            }
+        }
+
+        return neighbours;
     }
 
     public Node NodeFromWorldPoint(Vector3 worldPosition)
