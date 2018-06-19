fcc065b7a5a2d3373ac86beb94090e99d4b3238a
cblan <cblan002@gmail.com>
2018-06-19 12:36:13 +1200

    grenade feature

---
+++ Assets/GrenadeThrower.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrower : MonoBehaviour {
+
+    public GameObject grenade;
+    private GameObject thrownGrenade;
+    public AnimationCurve powerCurve;
+    float throwPower;
+    public float maxThrowPower;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(CalculateThrowStrength());
+        }
+	}
+
+    IEnumerator CalculateThrowStrength() //increases hitpower overtime.
+    {
+        float CurveTime = 0f;
+        float CurvePosition = 0f;
+        throwPower = 0f;
+        while (Input.GetButton("Fire1"))
+        {
+            CurveTime += Time.deltaTime *0.1f;
+            CurvePosition = powerCurve.Evaluate(CurveTime);
+            throwPower = maxThrowPower * CurvePosition;
+            yield return null;
+        }
+        thrownGrenade = Instantiate(grenade, transform.position, Quaternion.identity);
+        thrownGrenade.GetComponent<Rigidbody>().AddForce(transform.forward * throwPower, ForceMode.Impulse);
+        yield break;
+    }
+}
---
+++ Assets/StickyGrenade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyGrenade : MonoBehaviour {
+
+    private bool hasCollided = false;
+    public float explodeTime = 5;
+    public GameObject explosion;
+    private Rigidbody rB;
+
+    private void Start()
+    {
+        rB = GetComponent<Rigidbody>();
+        StartCoroutine(Explode());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!hasCollided)
+        {
+            GameObject emptyObject = new GameObject();
+            emptyObject.transform.SetParent(collision.gameObject.transform);
+            gameObject.transform.SetParent(emptyObject.transform);
+            rB.isKinematic = true;
+            hasCollided = true;
+        }
+    }
+
+    IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(explodeTime);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        yield break;
+    }
+}
