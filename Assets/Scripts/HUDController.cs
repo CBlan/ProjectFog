5964559db5f14fc6f84033068ca2c625a6ab187c
cblan <cblan002@gmail.com>
2018-09-28 15:40:48 +1200

    player UI

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class HUDController : MonoBehaviour {
 
-    public Image healthBar;
+    public Image healthBar1;
+    public Image healthBar2;
     public Image jetFuelBar;
-    public Image dashCooldownBar;
+    public Image oxygenBar;
+    public Image dashCooldownBar1;
+    public Image dashCooldownBar2;
 
     public Image damageImage;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
@@ -24,7 +27,10 @@ public class HUDController : MonoBehaviour {
 
     private void Update()
     {
-        healthBar.fillAmount = GameManager.instance.playerHP / GameManager.instance.maxPlayerHP;
+        healthBar1.fillAmount = GameManager.instance.playerHP / GameManager.instance.maxPlayerHP;
+        healthBar2.fillAmount = GameManager.instance.playerHP / GameManager.instance.maxPlayerHP;
+
+        oxygenBar.fillAmount = GameManager.instance.oxygen / GameManager.instance.maxOxygen;
 
         jetFuelBar.fillAmount = playerScript.jetFuel / playerScript.maxJetpackFuel;
 
@@ -46,15 +52,17 @@ public class HUDController : MonoBehaviour {
 
     IEnumerator DashCooldownBar()
     {
-        dashCooldownBar.fillAmount = 0;
+        dashCooldownBar1.fillAmount = 0;
+        dashCooldownBar2.fillAmount = 0;
         float timeRunning = 0;
         while (true)
         {
             timeRunning += Time.deltaTime;
-            dashCooldownBar.fillAmount = timeRunning / playerScript.dashCooldown;
+            dashCooldownBar1.fillAmount = timeRunning / playerScript.dashCooldown;
+            dashCooldownBar2.fillAmount = timeRunning / playerScript.dashCooldown;
             //print(timeRunning + " / " + playerScript.dashCooldown + " = " + dashCooldownBar.fillAmount);
 
-            if(dashCooldownBar.fillAmount >= 1)
+            if (dashCooldownBar1.fillAmount >= 1)
             {
                 dashed = false;
                 yield break;
