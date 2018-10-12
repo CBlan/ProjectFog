c2d03d2048eaf0c152e2d50375f375e3cdcda9ad
cblan <cblan002@gmail.com>
2018-10-12 15:29:26 +1300

    menu fix

--- Assets/MenuFunctions.cs
+++ Assets/MenuFunctions.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuFunctions : MonoBehaviour {
 
+    private void Start()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Main");
