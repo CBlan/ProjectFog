4041144b1d403931aef567259d51b22109230cb9
cblan <cblan002@gmail.com>
2018-10-18 14:28:25 +1300

    Tutorial

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject[] tutorialPannels;
+    private int activeTutorialPannel;
+    public static Tutorial controller;
+
+    private void Awake()
+    {
+        controller = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+
+        foreach (GameObject panel in tutorialPannels)
+        {
+            panel.SetActive(false);
+        }
+        tutorialPannels[0].SetActive(true);
+        activeTutorialPannel = 0;
+    }
+	
+    public void NextPannel()
+    {
+        tutorialPannels[activeTutorialPannel].SetActive(false);
+        activeTutorialPannel++;
+        if (activeTutorialPannel > tutorialPannels.Length-1)
+        {
+            print("Tutorial Finished");
+        }
+        else
+        {
+            tutorialPannels[activeTutorialPannel].SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/Tutorial_Movment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_Movment : MonoBehaviour {
+
+    private bool buttonPressed;
+
+    public GameObject indicator;
+
+    private void Start()
+    {
+        Instantiate(indicator, Tutorial.controller.player.transform.position, Tutorial.controller.player.transform.rotation);
+    }
+
+    void Update () {
+        if (Input.GetAxis("Horizontal") != 0 && !buttonPressed || Input.GetAxis("Vertical") != 0 && !buttonPressed)
+        {
+            Tutorial.controller.NextPannel();
+            buttonPressed = true;
+        }
+    }
+}
