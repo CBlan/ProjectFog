71dd9537c08cff1fe13a58ede4014211af088c3a
cblan <cblan002@gmail.com>
2018-11-23 15:36:45 +1300

    icon and logo

---
+++ Assets/Scripts/OxygenDrop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenDrop : MonoBehaviour {
+
+    public GameObject particles;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameManager.instance.oxygen = GameManager.instance.maxOxygen;
+            Instantiate(particles, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
